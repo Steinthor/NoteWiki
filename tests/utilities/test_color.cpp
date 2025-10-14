4d08a0d79b5418c67d1de4e8c8dcd5cb3470d4c4
Steinthor Jasonarson <steinthor@gmail.com>
2025-10-14 19:36:20 +0200

    adding color tests

--- tests/utilities/test_color.cpp
+++ tests/utilities/test_color.cpp
@@ -4,8 +4,36 @@
 
 #include "color.h"
 
-TEST(ColorTest, red) {
+TEST(ColorTest, Red) {
     std::ostringstream test;
     test << color::BOLD;
     EXPECT_EQ(color::BOLD, test.str());
+}
+
+TEST(ColorTest, DefaultFormatRed) {
+    std::string input = "Testing default color formatting";
+    std::string formatted_input = std::string(color::RED) + input + std::string(color::RESET);
+    std::string output = color::format(input);
+    EXPECT_EQ(formatted_input, output);
+}
+
+TEST(ColorTest, CustomFormatNoReset) {
+    std::string input = "Testing color formatting";
+    std::string formatted_input = std::string(color::RED) + input;
+    std::string output = color::format(input, {color::RED}, false);
+    EXPECT_EQ(formatted_input, output);
+}
+
+TEST(ColorTest, CustomFormatBlue) {
+    std::string input = "Testing color formatting";
+    std::string formatted_input = std::string(color::BLUE) + input + std::string(color::RESET);
+    std::string output = color::format(input, {color::BLUE});
+    EXPECT_EQ(formatted_input, output);
+}
+
+TEST(ColorTest, CustomFormatBlueBold) {
+    std::string input = "Testing color formatting";
+    std::string formatted_input = std::string(color::BLUE) + std::string(color::BOLD) + input + std::string(color::RESET);
+    std::string output = color::format(input, {color::BLUE, color::BOLD});
+    EXPECT_EQ(formatted_input, output);
 }
